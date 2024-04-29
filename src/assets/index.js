8253047e8d3bc82223442dcc5536a36340541e9f
akash kennedy <akashkennedy1@gmail.com>
2024-04-29 08:56:13 +0530

    Create Data for all Sections in data.js

---
+++ src/assets/index.js
@@ -0,0 +1,28 @@
+// Importing Partner Logos
+import logo1 from "../assets/logos/logo-1.png";
+import logo2 from "../assets/logos/logo-2.png";
+import logo3 from "../assets/logos/logo-3.png";
+import logo4 from "../assets/logos/logo-4.png";
+import logo5 from "../assets/logos/logo-5.png";
+import logo6 from "../assets/logos/logo-6.png";
+
+// Exporting Partner Logos
+export { logo1, logo2, logo3, logo4, logo5, logo6 };
+
+// Importing Features Icons
+import feature1 from "../assets/features/feature-1.png";
+import feature2 from "../assets/features/feature-2.png";
+import feature3 from "../assets/features/feature-3.png";
+import feature4 from "../assets/features/feature-4.png";
+
+// Exporting the Features  Icons
+export { feature1, feature2, feature3, feature4 };
+
+// Importing Blogs Wrapper Images
+import blog1 from "../assets/blogs/blogs-1.png";
+import blog2 from "../assets/blogs/blogs-2.png";
+import blog3 from "../assets/blogs/blogs-3.png";
+import blog4 from "../assets/blogs/blogs-4.png";
+
+// Exporting Blogs Wrapper Images
+export { blog1, blog2, blog3, blog4 };
---
+++ src/constants/data.js
@@ -0,0 +1,276 @@
+// Importing Hero Wrapper Image
+import HeroWrapper from "../assets/heroWrapper.png";
+
+// Importing Partner Logos
+import { logo1, logo2, logo3, logo4, logo5, logo6 } from "../assets/index.js";
+
+// Importing Features Icons
+import { feature1, feature2, feature3, feature4 } from "../assets/index.js";
+
+// Importing Blog Wapper Images
+import { blog1, blog2, blog3, blog4 } from "../assets/index.js";
+
+// Importing Business Analytics Wrapper Image
+import BusinessAnalyticsWrapper from "../assets/businessAnalytics.png";
+
+// Hero Data
+export const hero = {
+  heading: "Unleash the Power of Data",
+  info: "Step into the future with our state-of-the-art AI solutions. Unleash the potential of machine learning to innovate, optimize, and transform your business processes.",
+  wrapper: HeroWrapper,
+};
+
+// Partner Logos Data
+export const logos = [
+  {
+    id: 1,
+    url: logo1,
+    alt: "Placeholder Logo",
+  },
+  {
+    id: 2,
+    url: logo2,
+    alt: "Placeholder Logo",
+  },
+  {
+    id: 3,
+    url: logo3,
+    alt: "Placeholder Logo",
+  },
+  {
+    id: 4,
+    url: logo4,
+    alt: "Placeholder Logo",
+  },
+  {
+    id: 5,
+    url: logo5,
+    alt: "Placeholder Logo",
+  },
+  {
+    id: 6,
+    url: logo6,
+    alt: "Placeholder Logo",
+  },
+];
+
+// About Us Data
+export const aboutUs = {
+  heading: "Empowering Innovation AI",
+  info: "we are driven by the vision of transforming businesses with artificial intelligence. Founded in 2024, we have consistently pushed the boundaries of AI to offer smart, scalable, and intuitive solutions that drive growth and efficiency.",
+  info2:
+    "we are driven by the vision of transforming businesses with artificial intelligence. Founded in 2024, we have consistently pushed the boundaries of AI to offer smart, scalable, and intuitive solutions that drive growth and efficiency.",
+};
+
+// Stats Data in About Us Section
+export const stats = [
+  {
+    id: 1,
+    count: "32+",
+    desc: "Years in AI Innovation",
+  },
+  {
+    id: 2,
+    count: "20",
+    desc: "Clients Countries Worldwide",
+  },
+  {
+    id: 3,
+    count: "4000+",
+    desc: "Projects Successfully Implemented",
+  },
+];
+
+// Solutions Section Data
+export const solutions = {
+  heading: "Revolutionize Your Business with Our AI-Powered Features",
+};
+
+// Features Data In Solutions Section
+export const features = [
+  {
+    id: 1,
+    icon: feature1,
+    heading: "Advanced Data Analytics",
+    desc: "Predictive analytics to gain actionable insights and forecast future trends.",
+  },
+  {
+    id: 2,
+    icon: feature2,
+    heading: "Operations with Automation",
+    desc: "Enhance your operational efficiency with our AI-driven automated workflows.",
+  },
+  {
+    id: 3,
+    icon: feature3,
+    heading: "Unlock Insights with NLP",
+    desc: "Language processing to extract meaningful unstructured data.",
+  },
+  {
+    id: 4,
+    icon: feature4,
+    heading: "Custom AI for Your Needs",
+    desc: "Collaborate with our team of AI experts to build and deploy bespoke models.",
+  },
+];
+
+// Blogs Section Data
+export const blogs = {
+  heading: "In the spotlight",
+  info: "Stay updated with the latest trends, tips, and insights in business analytics. Explore our curated articles designed to empower your data-driven journey.",
+};
+
+// Blog Report Card Data
+export const blogReports = [
+  {
+    id: 1,
+    type: "Report",
+    title:
+      "Stay updated with the latest trends, tips, and insights in business analytics. Explore our curated articles designed to empower your data-driven journey.",
+    url: blog1,
+  },
+  {
+    id: 2,
+    type: "News",
+    title: "Customizing Your DataWise Dashboard: A Step-by-Step Guide",
+    url: blog2,
+  },
+  {
+    id: 3,
+    type: "News",
+    title: "Customizing Your DataWise Dashboard: A Step-by-Step Guide",
+    url: blog3,
+  },
+  {
+    id: 4,
+    type: "Report",
+    title: "Customizing Your DataWise Dashboard: A Step-by-Step Guide",
+    url: blog4,
+  },
+];
+
+// Business Analytics Section Data
+export const businessAnalytics = {
+  heading: "Experience the Future of Business Analytics",
+  desc: "Get hands-on with our advanced AI-driven features and see the difference for yourself. Start your free trial today.",
+  url: BusinessAnalyticsWrapper,
+  alt: "BusinessAnalyticsWrapper",
+};
+
+// Business Analytics Bullet Points Data
+export const businessAnalyticsPoints = [
+  {
+    id: 1,
+    text: "Context-aware natural language search and discovery",
+  },
+  {
+    id: 2,
+    text: "Embark on a journey of data-driven decision-making",
+  },
+  {
+    id: 3,
+    text: "Single permission model for data + AI",
+  },
+];
+
+// Frequently Asked Questions Data
+export const faq = {
+  heading: "Frequently asked questions",
+  desc: "Explore to learn more about how DataWise can empower your business with AI-driven solutions.",
+};
+
+// Individual FAQ Data
+export const faqs = [
+  {
+    id: 1,
+    title: "Can DataWise's AI solutions be  business systems?",
+    desc: "Yes, integration is a key strength of our AI solutions. DataWise's AI platform is designed for flexibility and can be integrated with a wide range of existing business systems.",
+  },
+  {
+    id: 2,
+    title: "Can your AI help improve my website's conversion rate?",
+    desc: "We pride ourselves on our service's adaptability, user-centric design, and our continual commitment to pushing the boundaries of AI technology.",
+  },
+  {
+    id: 3,
+    title: "What types of AI services does DataWise offer?",
+    desc: "Absolutely, our services are scalable and designed to accommodate and process large amounts of data efficiently.",
+  },
+  {
+    id: 4,
+    title: "Can your AI identify areas for A/B testing and personalization?",
+    desc: "We offer a range of support services from online resources, live chat support, to dedicated account representatives for enterprise customers.",
+  },
+  {
+    id: 5,
+    title: "What customer support do you offer for your AI solutions?",
+    desc: "Our services can benefit various industries, including healthcare, finance, retail, entertainment, and many more, wherever AI can be leveraged.",
+  },
+  {
+    id: 6,
+    title: "How can your AI help improve my website's SEO ranking?",
+    desc: "Yes, our platform allows for custom model training with your proprietary datasets.",
+  },
+];
+
+// Footer About Column Data
+export const footerAbout = [
+  {
+    id: 1,
+    title: "Company Overview",
+  },
+  {
+    id: 2,
+    title: "Careers",
+  },
+  {
+    id: 3,
+    title: "Press & Media",
+  },
+  {
+    id: 4,
+    title: "Testimonials",
+  },
+];
+
+// Footer Resources Column Data
+export const footerResources = [
+  {
+    id: 1,
+    title: "Blog",
+  },
+  {
+    id: 2,
+    title: "Help Center",
+  },
+  {
+    id: 3,
+    title: "Webinar & Events",
+  },
+  {
+    id: 4,
+    title: "Case Studies",
+  },
+];
+
+// Footer Support & Contact Column Data
+export const footerSupport = [
+  {
+    id: 1,
+    title: "Contact Us",
+  },
+  {
+    id: 2,
+    title: "Technical Support",
+  },
+  {
+    id: 3,
+    title: "Feedback",
+  },
+  {
+    id: 4,
+    title: "Community Forum",
+  },
+];
+
+// The Variables with array of data can be mapped using Javascript's Built in array.map() method.
